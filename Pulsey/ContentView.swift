25168050d8945e3282c23b3367463e4b61bd4ded
pascal <pascal.a.marter@gmail.com>
2024-10-14 20:43:25 +0200

    Move TabView into separate file MainTabView, to clean up ContentView

--- Pulsey/ContentView.swift
+++ Pulsey/ContentView.swift
@@ -11,43 +11,8 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var selectedTab = 0
-    
     var body: some View {
-        TabView(selection: $selectedTab) {
-            EntryView()
-                .tabItem {
-                    Image(systemName: "pencil.and.list.clipboard")
-                    Text("Entries")
-                }
-                .tag(0)
-                .accentColor(.purple)
-
-            
-            StatisticsView()
-                .tabItem {
-                    Image(systemName: "chart.dots.scatter")
-                    Text("Statistics")
-                }
-                .tag(1)
-                .accentColor(.purple)
-            
-            TrendsView()
-                .tabItem {
-                    Image(systemName: "chart.line.uptrend.xyaxis")
-                    Text("Trends")
-                }
-                .tag(2)
-                .accentColor(.purple)
-            
-            SettingsView()
-                .tabItem {
-                    Image(systemName: "gear")
-                    Text("Settings")
-                }
-                .tag(3)
-                .accentColor(.purple)
-        }
+        MainTabView()
         
     }
 }
