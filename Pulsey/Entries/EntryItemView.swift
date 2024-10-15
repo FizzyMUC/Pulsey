284b8b92fd969cf97ceaad3af7e48e6eff6a94bb
pascal <pascal.a.marter@gmail.com>
2024-10-15 15:26:05 +0200

    Add EntryItemView as separate file to style Cards for User presentation (testing!)

---
+++ Pulsey/Entries/EntryItemView.swift
@@ -0,0 +1,56 @@
+//
+//  EntryItemView.swift
+//  Pulsey
+//
+//  Created by Pascal Marter on 15.10.24.
+//
+
+import SwiftUI
+
+struct EntryItemView: View {
+    
+    let item: BloodValueItem
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            VStack {
+                HStack {
+                    Text(item.date, format: .dateTime.day().month().year())
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Text(item.date, format: .dateTime.hour().minute())
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+                .font(.caption)
+                .foregroundStyle(.secondary)
+                
+                HStack {
+                    Text("\(item.valueSystolic) / \(item.valueDiastolic)")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .font(.title)
+                    Text("Pulse: \(item.pulse)")
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .foregroundColor(.secondary)
+                }
+                
+                HStack {
+                    Text("\(item.assessment)")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .font(.subheadline)
+                    Text("\(item.icon)")
+                        .frame(alignment: .trailing)
+                }
+                
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+        .background(.gray.gradient.opacity(0.1))
+        .clipShape(RoundedRectangle(cornerRadius: 16))
+    }
+}
+
+
+#Preview {
+    let item = BloodValueItem(date: Date.now, valueSystolic: 130, valueDiastolic: 80, pulse: 66, note: "", assessment: "High normal", icon: "😍")
+    EntryItemView(item: item)
+}
--- Pulsey/Entries/EntryListView.swift
+++ Pulsey/Entries/EntryListView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct EntryListView: View {
     
-    //let bloodValues = Entries()
     let bloodValues: Entries
     
     var body: some View {
@@ -18,36 +17,37 @@ struct EntryListView: View {
                 NavigationLink {
                     // EditEntryView(bloodValues: bloodValues)
                 } label: {
-                    VStack(alignment: .leading) {
-                        VStack {
-                            HStack {
-                                Text(item.date, format: .dateTime.day().month().year())
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                Text(item.date, format: .dateTime.hour().minute())
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                            }
-                            .font(.caption)
-                            .foregroundStyle(.secondary)
-                            
-                            HStack {
-                                Text("\(item.valueSystolic) / \(item.valueDiastolic)")
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .font(.title2)
-                                Text("Pulse: \(item.pulse)")
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                                    .foregroundColor(.secondary)
-                            }
-                            
-                            HStack {
-                                Text("\(item.assessment)")
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .font(.subheadline)
-                                Text("\(item.icon)")
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                            }
-                            
-                        }
-                    }
+                    EntryItemView(item: item)
+//                    VStack(alignment: .leading) {
+//                        VStack {
+//                            HStack {
+//                                Text(item.date, format: .dateTime.day().month().year())
+//                                    .frame(maxWidth: .infinity, alignment: .leading)
+//                                Text(item.date, format: .dateTime.hour().minute())
+//                                    .frame(maxWidth: .infinity, alignment: .trailing)
+//                            }
+//                            .font(.caption)
+//                            .foregroundStyle(.secondary)
+//                            
+//                            HStack {
+//                                Text("\(item.valueSystolic) / \(item.valueDiastolic)")
+//                                    .frame(maxWidth: .infinity, alignment: .leading)
+//                                    .font(.title2)
+//                                Text("Pulse: \(item.pulse)")
+//                                    .frame(maxWidth: .infinity, alignment: .trailing)
+//                                    .foregroundColor(.secondary)
+//                            }
+//                            
+//                            HStack {
+//                                Text("\(item.assessment)")
+//                                    .frame(maxWidth: .infinity, alignment: .leading)
+//                                    .font(.subheadline)
+//                                Text("\(item.icon)")
+//                                    .frame(alignment: .trailing)
+//                            }
+//                            
+//                        }
+//                    }
                 }
             }
             .onDelete(perform: removeItems)
